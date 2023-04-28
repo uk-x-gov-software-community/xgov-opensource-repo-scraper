45854544de98a1a7b9efe892a9e8189bf70a1cb3
Chris Nesbitt-Smith <chris@cns.me.uk>
2023-04-28 19:43:56 +0100

    really slow

--- index.js
+++ index.js
@@ -70,7 +70,7 @@ const allReposForAllUKDepartments = await Promise.mapSeries(
 );
 
 const fetchAllSboms = async (repo) => {
-  await Promise.delay(1700); //slow down to appease github rate limiting
+  await Promise.delay(5000); //slow down to appease github rate limiting
   console.log(`Collecting SBOM for ${repo.owner.login}/${repo.name}`);
   try {
     return (
