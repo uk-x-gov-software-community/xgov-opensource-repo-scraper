09a42de405595c7b28234703e3c7b69d80093af4
Chris Nesbitt-Smith <chris@cns.me.uk>
2024-10-07 17:08:46 +0100

    replicate existing behavior, with try/catch to accommodate empty orgs

--- index.js
+++ index.js
@@ -4,12 +4,66 @@ import yaml from "js-yaml";
 import Octokat from "octokat";
 import Promise from "bluebird";
 import { writeFileSync } from "fs";
+import { Command } from "commander";
+
+const program = new Command();
+
+async function getOrgs() {
+  const allDepartments = yaml.safeLoad(
+    await (
+      await fetch(
+        "https://raw.githubusercontent.com/github/government.github.com/gh-pages/_data/governments.yml"
+      )
+    ).text()
+  );
+  return [].concat(
+    allDepartments["U.K. Councils"],
+    allDepartments["U.K. Central"]
+  );
+}
 
 const octo = new Octokat({
   token: process.env.GITHUB_TOKEN,
 });
 
-const formatResult = (result) => {
+async function outputIt(data, outputFile) {
+  if (outputFile) {
+    writeFileSync(outputFile, JSON.stringify(await data, null, 2));
+  } else {
+    process.stdout.write(JSON.stringify(await data, null, 2));
+  }
+}
+
+program
+  .command("get-orgs")
+  .description(
+    "Get a list of all the UK Government departments and agencies from GitHub"
+  )
+  .option("-w, --write <outputfile>", "Save output to a file")
+  .action(async (options) => outputIt(getOrgs(), options.write));
+
+program
+  .command("get-repos")
+  .argument("<org>", "The GitHub organisation to get the repos for")
+  .description("Get a list of all the repositories for a given org GitHub")
+  .option("-w, --write <outputfile>", "Save output to a file")
+  .action(async (org, options) => outputIt(getRepos(org), options.write));
+
+program
+  .command("get-all")
+  .description(
+    "Get consolodated data for all UK Government departments and agencies"
+  )
+  .action(async (options) =>
+    outputIt(
+      (await Promise.all(await getOrgs()).map(await getRepos)).flat(),
+      options.write
+    )
+  );
+
+program.parse();
+
+function formatRepoResult(result) {
   return {
     owner: result.owner.login,
     name: result.name,
@@ -17,50 +71,34 @@ const formatResult = (result) => {
     archived: result.archived,
     license: result.license,
     stargazersCount: result.stargazersCount,
-    // watchersCount: result.watchersCount, // github api appears to be returning the same as the stargazersCount?!
     language: result.language,
     forksCount: result.forksCount,
     openIssuesCount: result.openIssuesCount,
   };
-};
+}
 
-const fetchAll = async (org, args) => {
-  let response = await octo.orgs(org).repos.fetch({ per_page: 100 });
-  let aggregate = [response];
+async function fetchAll(org) {
+  let aggregate = [];
+  try {
+    let response = await octo.orgs(org).repos.fetch({ per_page: 100 });
+    aggregate = [response];
 
-  console.log(`fetched page 1 for ${org}`);
-  let i = 1;
-  await Promise.delay(50); //slow down to appease github rate limiting
-  while (response.nextPage) {
-    i++;
-    response = await response.nextPage();
-    console.log(`fetched page ${i} for ${org}`);
-    await Promise.delay(50); //slow down to appease github rate limiting
-    aggregate.push(response);
+    console.log(`fetched page 1 for ${org}`);
+    let i = 1;
+    await Promise.delay(50);
+    while (response.nextPage) {
+      i++;
+      response = await response.nextPage();
+      console.log(`fetched page ${i} for ${org}`);
+      await Promise.delay(50);
+      aggregate.push(response);
+    }
+  } catch (error) {
+    console.error(error);
   }
   return aggregate;
-};
+}
 
-const allDepartments = yaml.safeLoad(
-  await (
-    await fetch(
-      "https://raw.githubusercontent.com/github/government.github.com/gh-pages/_data/governments.yml"
-    )
-  ).text()
-);
-
-const UKDepartments = [].concat(
-  allDepartments["U.K. Councils"],
-  allDepartments["U.K. Central"]
-);
-
-const allReposForAllUKDepartments = await Promise.mapSeries(
-  UKDepartments,
-  fetchAll
-);
-
-const formattedResults = allReposForAllUKDepartments.flat(2).map(formatResult);
-
-console.log("writing results to file");
-writeFileSync("./public/repos.json", JSON.stringify(formattedResults));
-console.log("done");
+async function getRepos(org) {
+  return (await fetchAll(org)).flat().map(formatRepoResult);
+}
